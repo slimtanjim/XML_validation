9fc8e0d70e94b6ff8b6b126e75f86c4954c79f3b
slimtanjim <tanjim2051@gmail.com>
2023-04-19 07:52:46 -0700

    Add project files.

---
+++ Default.aspx
@@ -0,0 +1,54 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="hw4_part2_WebApplication.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <div>
+                <h1>Web Services to Process XML and XSD</h1>
+            </div>
+
+            <div>
+                <h4>Please enter the following information for XML varification 
+                 with correspondiong XSD file </h4>
+                
+                <asp:Label ID="Label1" runat="server" Text="XML URL (ex. https://www.public.asu.edu/~stanjim/Restaurants.xml ) "></asp:Label>
+                <br /><asp:TextBox ID="TextBox1" width="450" runat="server"></asp:TextBox>
+                
+                <br />
+                <asp:Label ID="Label2" runat="server" Text="XSD URL (ex. https://www.public.asu.edu/~stanjim/Restaurants.xsd)"></asp:Label>
+                <br /><asp:TextBox ID="TextBox2"  width="450" runat="server"></asp:TextBox>
+                <br /><br />
+                &nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;
+                <asp:Button ID="Button1" runat="server" Text="Validate XML Document" OnClick="Button1_Click" />
+
+                <br />
+                  &nbsp; &nbsp;<asp:Label ID="Label3" runat="server" Text="Press validate to see the result "></asp:Label>
+            </div>
+
+            <div>
+                <h4>Please enter the following information for XPathSearch</h4>
+
+                <asp:Label ID="Label4" runat="server" Text="XML URL : (ex. https://www.public.asu.edu/~stanjim/Restaurants.xml )"></asp:Label>
+             <br /><asp:TextBox ID="TextBox3"  width="450" runat="server"></asp:TextBox>
+
+                <br />  
+                <asp:Label ID="Label5" runat="server" Text="XPath Expression : (ex. /Restaurants/Restaurant[4]/Website/@facebook)"></asp:Label>
+                <br /><asp:TextBox ID="TextBox4"  width="450" runat="server"></asp:TextBox>
+                <br /><br />
+                &nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;
+                <asp:Button ID="Button2" runat="server" Text="Test Query" OnClick="Button2_Click" />
+                <br />          
+                <asp:Label ID="Label6" runat="server" Text="Press Search to see the result "></asp:Label>
+
+            </div>
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+
+namespace hw4_part2_WebApplication
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        // xml validation
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            // Get XML and XSD URLs from input fields
+            string xmlUrl = TextBox1.Text;
+            string xsdUrl = TextBox2.Text;
+
+            // Download XML and XSD files
+            string xmlContent = DownloadFile(xmlUrl);
+            string xsdContent = DownloadFile(xsdUrl);
+
+            // Load XML and XSD files
+            XmlReaderSettings settings = new XmlReaderSettings();
+            settings.ValidationType = ValidationType.Schema;
+            settings.Schemas.Add(null, XmlReader.Create(new StringReader(xsdContent)));
+            XmlReader xmlReader = XmlReader.Create(new StringReader(xmlContent), settings);
+
+            // Validate XML against XSD
+            bool isValid = true;
+            try
+            {
+                while (xmlReader.Read()) { }
+            }
+            catch (XmlException ex)
+            {
+                isValid = false;
+                Label3.Text = $"Error: {ex.Message}, line {ex.LineNumber}, column {ex.LinePosition}";
+            }
+
+            if (isValid)
+            {
+                Label3.Text = "No Error";
+            }
+        }
+
+        //xml quarries
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            // Get XML URL and path expression from input fields
+            string xmlUrl = TextBox3.Text;
+            string pathExpr = TextBox4.Text;
+
+            // Download XML file
+            string xmlContent = DownloadFile(xmlUrl);
+
+            // Load XML file
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.LoadXml(xmlContent);
+
+            // Evaluate path expression and get result
+            XmlNodeList resultNodes = xmlDoc.SelectNodes(pathExpr);
+            string result = "";
+            foreach (XmlNode node in resultNodes)
+            {
+                result += node.OuterXml + "\n";
+            }
+
+            // Display result
+            Label6.Text = result;
+        }
+
+        // Download file from URL
+        private string DownloadFile(string url)
+        {
+            //refresh string content
+            string content = "";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            {
+                using (Stream stream = response.GetResponseStream())
+                {
+                    using (StreamReader reader = new StreamReader(stream))
+                    {
+                        content = reader.ReadToEnd();
+                    }
+                }
+            }
+            return content;
+        }
+
+
+    }
+}
